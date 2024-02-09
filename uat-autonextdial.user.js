ccb538018dd12fb60b99c20799db9c4b00184db6
AlbanDugueOrnikar <151523549+AlbanDugueOrnikar@users.noreply.github.com>
2024-02-09 14:14:21 +0100

    Update uat-autonextdial.user.js

--- uat-autonextdial.user.js
+++ uat-autonextdial.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UAT Auto Next Dialler
 // @namespace    Ornikar-Salesforce
-// @version      1.3
+// @version      1.4
 // @downloadURL  https://github.com/ornikar/script-next-automatic-dialler/raw/main/uat-autonextdial.user.js
 // @updateURL    https://github.com/ornikar/script-next-automatic-dialler/raw/main/uat-autonextdial.user.js
 // @description  Automatically click the next button on the dialler component every 5 seconds
