c30c3522b7e42d735999b284ba5c9f2167cf97d4
AlbanDugueOrnikar <151523549+AlbanDugueOrnikar@users.noreply.github.com>
2024-02-14 12:18:04 +0100

    block next if user is not online

--- uat-autonextdial.user.js
+++ uat-autonextdial.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UAT Auto Next Dialler
 // @namespace    Ornikar-Salesforce
-// @version      1.4
+// @version      1.5
 // @downloadURL  https://github.com/ornikar/script-next-automatic-dialler/raw/main/uat-autonextdial.user.js
 // @updateURL    https://github.com/ornikar/script-next-automatic-dialler/raw/main/uat-autonextdial.user.js
 // @description  Automatically click the next button on the dialler component every 20 seconds
@@ -11,26 +11,30 @@
 // @grant        none
 // ==/UserScript==
 
-// Date : 09-02-2024
+// Date : 14-02-2024
 
 (function() {
     'use strict';
-  
+
     const delayClickNextButton = 20000; // 20 seconds
     const delayCheckScriptActivity = 30000; // 30 seconds
 
     // Define a function that tries to find the next button and click it
     function clickNextButton() {
-        // Try to find the next button by its selector
-        let buttonNext = document.querySelector("button.slds-button.slds-button--neutral.moveToNextButton.uiButton");
-        // Check if the next button exists
-        if (buttonNext) {
-            // Click the next button, log a message
-            buttonNext.click();
-            console.log("Button next clicked");
-        } else {
-            // Log a message that the next button is not found
-            console.log("Button next not found");
+        let isUserOnline = checkOnlineStatus();
+
+        if (isUserOnline) {
+            // Try to find the next button by its selector
+            let buttonNext = document.querySelector("button.slds-button.slds-button--neutral.moveToNextButton.uiButton");
+            // Check if the next button exists
+            if (buttonNext) {
+                // Click the next button, log a message
+                buttonNext.click();
+                console.log("Button next clicked");
+            } else {
+                // Log a message that the next button is not found
+                console.log("Button next not found");
+            }
         }
     }
 
@@ -46,6 +50,23 @@
             console.log("Button check script user activity not found");
         }
     }
+
+    // Function that checks if the user is online before clicking on the next button
+    function checkOnlineStatus() {
+    // Try to find the span with the title "Online Status".
+    let span = document.querySelector('span[title="Online Status"]');
+    // Check if the span exists
+    if (span) {
+        // If found, return true
+        console.log("Element with 'Online Status' found");
+        return true;
+    } else {
+        // If not found, return false
+        console.log("Element with 'Online Status' not found");
+        return false;
+    }
+}
+
 
     // Set different intervals for the functions
     let intervalClickNext = setInterval(clickNextButton, delayClickNextButton);
